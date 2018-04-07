2db9f63d780c4c162d192d5f2721cad3b4c524ea
Tomasz Sokolowski <sokolowskit@gmail.com>
2018-04-07 22:53:15 +0200

    Add html css and JS-jQ files

---
+++ App.js
@@ -0,0 +1,32 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '2892',
+  'X-Auth-Token': 'f2336f184740a1b7811b9cb109b2c918'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(cardObj);
+  	})
+}
